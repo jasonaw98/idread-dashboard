0f92f373a1c28b7f12d2bc53720869eb693c625e
Jason Aw <jason_aw1998@hotmail.com>
2024-09-09 17:35:06 +0800

    add export excel

--- components/chat-dashboard.tsx
+++ components/chat-dashboard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
+import * as XLSX from "xlsx";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -25,10 +26,64 @@ import { cleanedData } from "@/data/output";
 import { MonthChart } from "./MonthChart";
 import { HourChart } from "./HourChart";
 
+interface Message {
+  Account: string;
+  Incoming: string;
+  IncomingTime: string;
+  Outgoing: string;
+  OutgoingTime: string;
+}
+
 export function ChatDashboard() {
   const [selectedTimeRange, setSelectedTimeRange] = useState("7d");
+  const [loading, setLoading] = useState(false);
   const { uniqueUsers, totalMessages, recipientMessageCountArray } =
     calculateMessageStats();
+
+  const [selectedItems, setSelectedItems] = useState<Message[]>([]);
+
+  const handleCheckboxChange = useCallback(
+    (message: Message, isChecked: boolean) => {
+      setSelectedItems((prevSelected) => {
+        const updatedSelected = isChecked
+          ? [...prevSelected, message]
+          : prevSelected.filter((item) => item !== message);
+
+        return updatedSelected;
+      });
+    },
+    []
+  );
+
+  const ExportXlsx = async (title?: string, worksheetname?: string) => {
+    try {
+      setLoading(true);
+      // Check if the action result contains data and if it's an array
+      if (selectedItems && Array.isArray(selectedItems)) {
+        const dataToExport = selectedItems.map((pro: any) => ({
+          User: pro.Account,
+          TimeSent: pro.IncomingTime,
+          MessageSent: pro.Incoming,
+          MessageReceived: pro.Outgoing,
+          TimeReceived: pro.OutgoingTime,
+        }));
+        // Create Excel workbook and worksheet
+        const workbook = XLSX.utils.book_new();
+        const worksheet = XLSX.utils?.json_to_sheet(dataToExport);
+        XLSX.utils.book_append_sheet(workbook, worksheet, worksheetname);
+        // Save the workbook as an Excel file
+        XLSX.writeFile(workbook, `${title}.xlsx`);
+        console.log(`Exported data to ${title}.xlsx`);
+        setLoading(false);
+      } else {
+        setLoading(false);
+        console.log("#==================Export Error");
+      }
+    } catch (error: any) {
+      setLoading(false);
+      console.log("#==================Export Error", error.message);
+    }
+  };
 
   const sortedData = [...cleanedData].sort((a, b) => {
     return (
@@ -59,7 +114,7 @@ export function ChatDashboard() {
 
   useEffect(() => {
     refPagination.current?.scrollIntoView({ behavior: "smooth" });
-  }, [nextPage, prevPage, currentPage]);
+  }, [currentPage]);
 
   return (
     <Tabs defaultValue="dashboard">
@@ -247,9 +302,18 @@ export function ChatDashboard() {
           <div className="flex flex-1 flex-col p-8 overflow-auto w-full">
             <header className="flex justify-between items-center mb-8">
               <h1 className="text-3xl font-bold">Messages</h1>
+              <button
+                onClick={() => ExportXlsx("Messages", "MessageExport")}
+                className="group relative h-12 overflow-hidden rounded-md bg-blue-500 px-6 text-neutral-50 transition hover:bg-blue-600 font-semibold"
+              >
+                <span className="relative">
+                  {loading ? "Loading..." : "Export"}
+                </span>
+              </button>
             </header>
             <div className="bg-white rounded-xl p-4 shadow w-full flex flex-col">
-              <div className="grid grid-cols-[1fr_1fr_2fr_2fr_1fr] gap-4 px-4">
+              <div className="grid grid-cols-[0.3fr_1fr_1fr_2fr_2fr_1fr] gap-4 px-4">
+                <h1 className="font-semibold">{''}</h1>
                 <h1 className="font-semibold">Users</h1>
                 <h1 className="font-semibold">Time Sent</h1>
                 <h1 className="font-semibold">Message Sent</h1>
@@ -260,8 +324,15 @@ export function ChatDashboard() {
                 {currentItems.map((message, index) => (
                   <div
                     key={index}
-                    className="p-4 bg-gray-50 rounded-lg grid grid-cols- grid-cols-[1fr_1fr_2fr_2fr_1fr] gap-2 border border-border"
+                    className="p-4 bg-gray-50 rounded-lg grid grid-cols- grid-cols-[0.3fr_1fr_1fr_2fr_2fr_1fr] gap-2 border border-border"
                   >
+                    <input
+                      type="checkbox"
+                      className="w-4 h-4"
+                      onChange={(e) =>
+                        handleCheckboxChange(message, e.target.checked)
+                      }
+                    />
                     <p className="text-gray-700 ">{message.Account}</p>
                     <p className="text-gray-700 ">
                       {new Date(message.IncomingTime).toLocaleString()}

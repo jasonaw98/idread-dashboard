cbcb6c875f8d4c4f76b420d7285df01d787994ce
jason <jason_aw1998@hotmail.com>
2024-09-19 12:30:17 +0800

    hashing

--- components/UserTable.tsx
+++ components/UserTable.tsx
@@ -22,8 +22,8 @@ export default async function UserTable() {
             {users &&
               users.map((user, index) => (
                 <tr key={index} className="border-t">
-                  <td className="py-2">{user.key}</td>
-                  <td className="py-2">{user.name}</td>
+                  <td className="py-2">{(user.key).toString().slice(0, 5) + '*****'}</td>
+                  <td className="py-2">{(user.name).toString().slice(0, 3) + '*****'}</td>
                   <td className="py-2">{user.value}</td>
                 </tr>
               ))}

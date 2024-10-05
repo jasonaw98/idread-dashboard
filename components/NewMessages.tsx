ad506416be56981652a4bfb69c93db88a08c0e06
jason <jason_aw1998@hotmail.com>
2024-10-05 16:27:42 +0800

    revamp

--- components/NewMessages.tsx
+++ components/NewMessages.tsx
@@ -19,7 +19,7 @@ const NewMessages = () => {
   const [selectedItems, setSelectedItems] = useState<Message[]>([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [searching, setSearching] = useState(false)
+  const [searching, setSearching] = useState(false);
   const pageSize = 6;
 
   const Search = () => {
@@ -31,7 +31,6 @@ const NewMessages = () => {
         setData(text);
         setLoading(false);
       };
-
       search();
     }, 500);
 
@@ -140,76 +139,162 @@ const NewMessages = () => {
         </button>
         <Search />
       </header>
-      <div className="bg-white rounded-xl p-4 shadow w-full flex flex-col">
-        <div className="grid grid-cols-[0.2fr_0.6fr_1fr_1fr_2fr_2fr_1fr] gap-4 px-4">
-          <h1 className="font-semibold">{""}</h1>
-          <h1 className="font-semibold">Contact</h1>
-          <h1 className="font-semibold">Name</h1>
-          <h1 className="font-semibold">Time Sent</h1>
-          <h1 className="font-semibold">Message Sent</h1>
-          <h1 className="font-semibold">Message Received</h1>
-          <h1 className="font-semibold  text-right">Time Received</h1>
-        </div>
-        <div className="space-y-2 mt-4 font-semibold">
-          {data &&
-            data.map((message, index) => (
-              <div
-                key={index}
-                className="p-4 bg-gray-50 rounded-lg grid grid-cols- grid-cols-[0.2fr_0.6fr_1fr_1fr_2fr_2fr_1fr] gap-2 border border-border"
-              >
-                <input
-                  type="checkbox"
-                  className="w-4 h-4"
-                  onChange={(e) =>
-                    handleCheckboxChange(message, e.target.checked)
-                  }
-                />
-                <p className="text-gray-700 ">
-                  {message.userId.toString().slice(0, 5) + "*****"}
-                </p>
-                <p className="text-gray-700 ">
-                  {message.userName.toString().slice(0, 3) + "*****"}
-                </p>
-                <p className="text-gray-700 ">
-                  {new Date(message.sent_at).toLocaleString("en-US", {
-                    hour12: true,
-                  })}
-                </p>
-                <p className="text-gray-700 break-words">
-                  {message.message_sent}
-                </p>
-                <p className="text-gray-700 text-justify break-words max-w-[450px]">
-                  {message.message_received}
-                </p>
-                <p className="text-gray-700 text-right">
-                  {new Date(message.received_at).toLocaleString("en-US", {
-                    hour12: true,
-                  })}
-                </p>
-              </div>
-            ))}
-        </div>
+      {true && (
+        <div>
+          <div className="bg-white pt-2 rounded-xl">
+            <table className="table-auto w-full">
+              <thead>
+                <tr>
+                  <th className="font-semibold">{""}</th>
+                  <th className="font-semibold">Contact</th>
+                  <th className="font-semibold">Name</th>
+                  <th className="font-semibold">Time Sent</th>
+                  <th className="font-semibold">Message Sent</th>
+                  <th className="font-semibold">Message Received</th>
+                  <th className="font-semibold">Time Received</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data &&
+                  data.map((message, index) => (
+                    <tr
+                      key={index}
+                      className="py-4 bg-slate-50 border-border border"
+                    >
+                      <td className="px-3">
+                        <input
+                          type="checkbox"
+                          className="w-5 h-5"
+                          onChange={(e) =>
+                            handleCheckboxChange(message, e.target.checked)
+                          }
+                        />
+                      </td>
+                      <td className="px-2 border border-border">
+                        {message.userId.toString().slice(0, 5) + "*****"}
+                      </td>
+                      <td className="px-2 border border-border">
+                        {message.userName.toString().slice(0, 3) + "*****"}
+                      </td>
+                      <td className="text-center min-w-24 border border-border">
+                        {new Date(message.sent_at).toLocaleString("en-US", {
+                          hour12: true,
+                        })}
+                      </td>
+                      <td className="break-words min-w-[300px] pl-2 border border-border">
+                        {message.message_sent}
+                      </td>
+                      <td className="text-justify break-words p-2 border border-border">
+                        {message.message_received}
+                      </td>
+                      <td className="text-center min-w-24">
+                        {new Date(message.received_at).toLocaleString("en-US", {
+                          hour12: true,
+                        })}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
 
-        {!searching && <div className="flex justify-center gap-8 mt-4">
-          <button
-            onClick={handlePrevPage}
-            disabled={currentPage === 1}
-            className={`group h-10 overflow-hidden rounded-md px-6 text-neutral-50 transition ${
-              currentPage === 1
-                ? "bg-gray-300"
-                : "bg-blue-500 hover:bg-blue-600"
-            } font-semibold`}
-          >
-            Previous
-          </button>
-          <button
-            onClick={handleNextPage}
-            className="group h-10 overflow-hidden rounded-md bg-blue-500 px-6 text-neutral-50 transition hover:bg-blue-600 font-semibold"
-          >
-            Next
-          </button>
-        </div>}
-      </div>
+          {!searching && (
+            <div className="flex justify-center gap-8 mt-4">
+              <button
+                onClick={handlePrevPage}
+                disabled={currentPage === 1}
+                className={`group h-10 overflow-hidden rounded-md px-6 text-neutral-50 transition ${
+                  currentPage === 1
+                    ? "bg-gray-300"
+                    : "bg-blue-500 hover:bg-blue-600"
+                } font-semibold`}
+              >
+                Previous
+              </button>
+              <button
+                onClick={handleNextPage}
+                className="group h-10 overflow-hidden rounded-md bg-blue-500 px-6 text-neutral-50 transition hover:bg-blue-600 font-semibold"
+              >
+                Next
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+      {false && (
+        <div className="bg-white rounded-xl p-4 shadow w-full flex flex-col">
+          <div className="grid grid-cols-[0.2fr_0.6fr_1fr_1fr_2fr_2fr_1fr] gap-4 px-4">
+            <h1 className="font-semibold">{""}</h1>
+            <h1 className="font-semibold">Contact</h1>
+            <h1 className="font-semibold">Name</h1>
+            <h1 className="font-semibold">Time Sent</h1>
+            <h1 className="font-semibold">Message Sent</h1>
+            <h1 className="font-semibold">Message Received</h1>
+            <h1 className="font-semibold">Time Received</h1>
+          </div>
+          <div className="space-y-2 mt-4 font-semibold">
+            {data &&
+              data?.map((message, index) => (
+                <div
+                  key={index}
+                  className="p-4 bg-gray-50 rounded-lg grid grid-cols- grid-cols-[0.2fr_0.4fr_0.5fr_1fr_2fr_minmax(500px,_3fr)_1fr] gap-2 border border-border"
+                >
+                  <input
+                    type="checkbox"
+                    className="w-4 h-4"
+                    onChange={(e) =>
+                      handleCheckboxChange(message, e.target.checked)
+                    }
+                  />
+                  <p className="text-gray-700 ">
+                    {message.userId.toString().slice(0, 5) + "*****"}
+                  </p>
+                  <p className="text-gray-700 ">
+                    {message.userName.toString().slice(0, 3) + "*****"}
+                  </p>
+                  <p className="text-gray-700 ">
+                    {new Date(message.sent_at).toLocaleString("en-US", {
+                      hour12: true,
+                    })}
+                  </p>
+                  <p className="text-gray-700 break-words">
+                    {message.message_sent}
+                  </p>
+                  <p className="text-gray-700 text-justify break-words max-w-[450px]">
+                    {message.message_received}
+                  </p>
+                  <p className="text-gray-700 text-right">
+                    {new Date(message.received_at).toLocaleString("en-US", {
+                      hour12: true,
+                    })}
+                  </p>
+                </div>
+              ))}
+          </div>
+
+          {!searching && (
+            <div className="flex justify-center gap-8 mt-4">
+              <button
+                onClick={handlePrevPage}
+                disabled={currentPage === 1}
+                className={`group h-10 overflow-hidden rounded-md px-6 text-neutral-50 transition ${
+                  currentPage === 1
+                    ? "bg-gray-300"
+                    : "bg-blue-500 hover:bg-blue-600"
+                } font-semibold`}
+              >
+                Previous
+              </button>
+              <button
+                onClick={handleNextPage}
+                className="group h-10 overflow-hidden rounded-md bg-blue-500 px-6 text-neutral-50 transition hover:bg-blue-600 font-semibold"
+              >
+                Next
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };

0991b84d9d440ab89e7a49603c78d5e806615e44
jason <jason_aw1998@hotmail.com>
2024-11-04 03:12:06 +0800

    fix patch

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -10,13 +10,13 @@ export async function createClient() {
     process.env.NEXT_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll()
+        async getAll() {
+          return (await cookieStore).getAll()
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
+            cookiesToSet.forEach(async ({ name, value, options }) =>
+              (await cookieStore).set(name, value, options)
             )
           } catch {
             // The `setAll` method was called from a Server Component.
